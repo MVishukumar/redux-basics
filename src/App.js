bee105f159cbff08e1713d5600974ea7638e3ac2
Vishukumar M <vishukumar.m@ericsson.com>
2020-05-30 20:44:28 +0530

    "initial commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./action/index";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>It works</p>
+      <p>Count: {counter}</p>
+      {isLogged ? <p>Sensitive information</p> : ""}
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { createStore } from "redux";
+import allReducers from "./reducer/index";
+import { Provider } from "react-redux";
+/*
+import { createStore } from "redux";
+
+//action
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+//action
+const decrement = () => {
+  return {
+    type: "DECCREMENT",
+  };
+};
+
+//reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECCREMENT":
+      return state - 1;
+
+    default:
+      break;
+  }
+};
+
+//store
+let store = createStore(counter);
+
+//display it in console
+store.subscribe(() => console.log(store.getState()));
+
+//dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
+//STORE -> Globalized state
+//Store holds all the data required for application
+//Store maintains the state of all the data
+//Simply store of data, globalized state
+
+//ACTION -> INCREMENT
+//Event that causes change of state in store
+
+//REDUCER ->
+//Responsible for changing the state in store
+
+//DISPATCH ->
+//Execute action
+
+*/
+
+const myStore = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={myStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
